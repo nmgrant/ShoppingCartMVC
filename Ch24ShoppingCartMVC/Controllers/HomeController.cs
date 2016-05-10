1db67cac5b7ba34645784b7b174b35b5dd53222f
Evan McNaughtan <evan4james@yahoo.com>
2016-05-10 15:21:06 -0700

    Revert "added cart"
    This reverts commit 232ebd98e649bdab8ef45b29febb6b00306d5ef7.

--- Ch24ShoppingCartMVC/Controllers/HomeController.cs
+++ Ch24ShoppingCartMVC/Controllers/HomeController.cs
@@ -6,19 +6,20 @@ using System.Web.Mvc;
 using Ch24ShoppingCartMVC.Models;
 
 namespace Ch24ShoppingCartMVC.Controllers {
-   public class HomeController : Controller {
-      [HttpGet]
-      public ViewResult Index() {
-         ViewBag.HeaderText = "Welcome to the Halloween Store";
-         ViewData["FooterText"] = "Where every day is Halloween!";
-         return View();
-      }
-      [HttpGet]
-      public ViewResult Contact() {
-         //CREATE A ContactViewModel OBJECT call model  model
-         ContactViewModel model = new ContactViewModel();
-         //Pass model to View
-         return View(model);
-      }
-   }
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index() {
+            ViewBag.HeaderText = "Welcome to the Halloween Store";
+            ViewData["FooterText"] = "Where every day is Halloween!";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult Contact() {
+            //CREATE A ContactViewModel OBJECT call model  model
+            ____________________________________________________
+            //Pass model to View
+            _________________________________
+        }
+    }
 }
