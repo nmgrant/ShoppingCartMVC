72a71714986d16e24f51b38613b7d744bc190a24
Nicholas <ngrant40@gmail.com>
2016-05-11 11:51:02 -0700

    Added checkout

---
+++ Ch24ShoppingCartMVC/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ch24ShoppingCartMVC.Models.ViewModels {
+	public class CheckoutViewModel {
+		public List<ProductViewModel> Cart { get; set; }
+		public const double TAX_RATE = 0.08;
+		public List<string> PaymentOptions {
+			get {
+				List<string> options = new List<string>;
+				options.Add("Visa");
+				options.Add("MasterCard");
+				options.Add("PayPal");
+				return options;
+			}
+		}
+	}
+}
